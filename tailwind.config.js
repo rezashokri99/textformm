9aa92133fdbf0bc94c1f392800855bc2458585ad
rezashokri99 <rezashokri131@gmail.com>
2022-06-12 16:01:37 +0430

    committ end

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,16 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
-    "./public/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.{html,js,ts,jsx,tsx}",
+    "./src/components/**/*.{html,js,ts,jsx,tsx}",
   ],
-  // theme: {
-  //   extend: {
-  //     fontFamily: {
-  //        sans: ['iranyekan', ...defaultTheme.fontFamily.sans],
-  //     },
-  //  },
-   
-  // },
   theme: {
-    fontFamily: {
-      iranyekan: ["iranyekan", "sans-serif"],
-    },
-
-    extend: {},
+    extend: {
+      fontFamily: {
+        iranyekan: ["iranyekan"],
+      },
+   },
+   
   },
   plugins: [],
 }
